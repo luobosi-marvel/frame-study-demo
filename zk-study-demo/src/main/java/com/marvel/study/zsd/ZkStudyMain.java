562a3e4eff1c1356e97e611cd0b3f14ef27c4fef
luobosi <luobosi@2dfire.com>
2018-09-21 01:26:00 +0800

    zk demo 练习，对zk上的节点进行增删改查

---
+++ zk-study-demo/src/main/java/com/marvel/study/zsd/ZkStudyMain.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2009-2018 Hangzhou 2Dfire Technology Co., Ltd. All rights reserved
+ */
+package com.marvel.study.zsd;
+
+/**
+ * ZkStudyMain
+ *
+ * @author luobosi@2dfire.com
+ * @since 2018-09-21
+ */
+public class ZkStudyMain {
+
+    public static void main(String[] args) {
+        System.out.println("启动");
+    }
+}
