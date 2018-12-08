cb0cc6ad1ac0405687cfdb32c7537ebfabad5f84
萝卜丝 <luobosi@2dfire.com>
2018-12-08 15:43:58 +0800

    Redis 版本延迟队列实现

---
+++ dyno-queues-demo/src/main/java/com/marvel/dyno/DynoQueuesDemoApplication.java
@@ -0,0 +1,12 @@
+package com.marvel.dyno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynoQueuesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynoQueuesDemoApplication.class, args);
+	}
+}
---
+++ dyno-queues-demo/src/test/java/com/marvel/dyno/DynoQueuesDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marvel.dyno;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DynoQueuesDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
